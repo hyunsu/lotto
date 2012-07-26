3545c526965654881b6bae4ea5f61b97c16e8240
hyunsu <nonno0323@gmail.com>
2012-07-26 12:16:53 +0900

    init

---
+++ src/Lotto.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+
+public class Lotto {
+
+	/**
+	 * @param args
+	 * new Random 인스턴스변수
+	 */
+	public static void main(String[] args) {
+		new Lotto().getLotto();
+	}
+	public int[] getLotto() {
+		int[] numbers = new int[6];
+		for (int i = 0; i < 6; i++) {
+			numbers[i] = getNumber();
+		}
+		return numbers;
+	}
+		
+	Random random = new Random();
+	private int getNumber() {
+		return random.nextInt(45) + 1;
+	}
+
+}
---
+++ src/Player.java
@@ -0,0 +1,15 @@
+
+public class Player {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Lotto lotto = new Lotto();
+		int[] numbers = lotto.getLotto();
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
+	}
+
+}
